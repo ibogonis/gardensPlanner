dcf77e82e058640b5b413e6ca6c192c7ac5288b9
Ira Bogonis <i.bogonis@gmail.com>
2024-06-12 13:34:59 +0300

    add History Component with an empty table

---
+++ src/components/HistoryTable.js
@@ -0,0 +1,14 @@
+export function HistoryTable() {
+  return (
+    <>
+      <h4>History</h4>
+      <table>
+        <tr>
+          <th>Year</th>
+          <th>Name</th>
+          <th>Image</th>
+        </tr>
+      </table>
+    </>
+  );
+}
